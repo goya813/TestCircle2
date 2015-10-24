96ff7c754cbda29b49684d664362c033e9021ab5
Goya Tomohiro <wasabibeef@gmail.com>
2015-10-24 18:02:25 +0900

    ActionbarActivityをインポートから外した

--- app/src/main/java/ci/circle/test/testcircle2/MainActivity.java
+++ app/src/main/java/ci/circle/test/testcircle2/MainActivity.java
@@ -1,7 +1,6 @@
 package ci.circle.test.testcircle2;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
